3b0e09bbf1c2c1adc45dbe6f322d19489af46d34
sbx_user1051 <sbx_user1051@169.254.49.229>
2024-04-11 11:34:01 +0000

    Thu Apr 11 11:34:01 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-private static final Map<Character, Integer> prices = Map.of(
-        'A', 50, 'B', 30, 'C', 20, 'D', 15, 'F', 10, 'G', 20,
+private static final Map<Character, Integer> prices = Map.ofEntries(
+        w'A', 50, 'B', 30, 'C', 20, 'D', 15, 'F', 10, 'G', 20,
         'H', 10, 'I', 35, 'J', 60, 'K', 80, 'L', 90, 'M', 15, 'N', 40, 'O', 10,
         'P', 50, 'Q', 30, 'R', 50, 'S', 30, 'T', 20, 'U', 40, 'V', 50, 'W', 20,
         'X', 90, 'Y', 10, 'Z', 50);
@@ -53,6 +53,7 @@ private static final Map<Character, Integer> prices = Map.of(
      return total;
     }
 }
+
 
 
 
