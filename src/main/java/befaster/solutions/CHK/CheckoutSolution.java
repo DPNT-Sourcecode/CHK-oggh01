a781974114ceb4d1305bb74bf3b3b6e53a1c5797
sbx_user1051 <sbx_user1051@169.254.49.229>
2024-04-11 10:43:21 +0000

    Thu Apr 11 10:43:21 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,11 +5,19 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
      char[] skusArray =  skus.toCharArray();
-     int countA, count
+     int countA = 0, countB = 0, countC = 0, countD = 0;
         for(char skusChr : skusArray){
-            switch (skusChr)
+            switch (skusChr){
+                case 'A' -> countA++;
+                case 'B' -> countB++;
+                case 'C' -> countC++;
+                case 'D' -> countD++;
+                default -> System.out.println("Product doesn't exist");
+            }
+        
         }
     }
 }
+
 
 
