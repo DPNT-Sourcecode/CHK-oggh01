728f6dd6eb4b567f45337e45c3c40c3b542c5b09
sbx_user1051 <sbx_user1051@169.254.49.229>
2024-04-11 11:43:01 +0000

    Thu Apr 11 11:43:01 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,6 +3,7 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
@@ -15,6 +16,19 @@ private static final Map<Character, Integer> prices = Map.ofEntries(
         new SimpleEntry<>('Q', 30), new SimpleEntry<>('R', 50), new SimpleEntry<>('S', 30), new SimpleEntry<>('T', 20),
         new SimpleEntry<>('U', 40), new SimpleEntry<>('V', 50), new SimpleEntry<>('W', 20), new SimpleEntry<>('X', 90),
         new SimpleEntry<>('Y', 10), new SimpleEntry<>('Z', 50));
+
+
+private static final Map<Character, Map<Integer, Integer>> offers = new HashMap<>();
+
+static{
+    offers.put('A', Map.of(3, 130, 5, 200));
+    offers.put('B', Map.of(2, 45));
+    offers.put('H', Map.of(5, 45, 10, 80));
+    offers.put('K', Map.of(2,150));
+    offers.put('P', Map.of(5,200));
+    offers.put('Q', Map.of(3,80));
+    offers.put('V', Map.of(2, 90, 3,130));
+}
     public Integer checkout(String skus) {
 
         int priceA = 50, priceB = 30, priceC = 20, priceD = 15, priceE = 40, priceF = 10;
@@ -57,3 +71,4 @@ private static final Map<Character, Integer> prices = Map.ofEntries(
      return total;
     }
 }
+
