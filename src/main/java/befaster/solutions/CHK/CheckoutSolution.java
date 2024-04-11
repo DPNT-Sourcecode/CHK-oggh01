37c0dc54b2e700132b2ba431a026e41481b55a14
sbx_user1051 <sbx_user1051@169.254.49.229>
2024-04-11 11:52:01 +0000

    Thu Apr 11 11:52:01 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -31,45 +31,25 @@ static{
 }
     public Integer checkout(String skus) {
 
-        int priceA = 50, priceB = 30, priceC = 20, priceD = 15, priceE = 40, priceF = 10;
-        int offerThreeA = 130, offerFiveA = 200, offerB = 45;
-        int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0, countF = 0;
+      Map<Character, Integer> basket = new HashMap<>();
+      for(char skusChr: skus.toCharArray()){
+          if(!prices.containsKey(skusChr))
+              return -1;
+          basket.merge(skusChr, 1, Integer::sum);
+      }
 
-        for(char skusChr : skus.toCharArray()){
-            switch (skusChr){
-                case 'A' -> countA++;
-                case 'B' -> countB++;
-                case 'C' -> countC++;
-                case 'D' -> countD++;
-                case 'E' -> countE++;
-                case 'F' -> countF++;
-                default -> { return -1; }
-            }
 
         }
 
-        int total = 0;
+       private void applySpecialOffers(Map<Character, Integer> basket){
 
-     total += (countA / 5) * offerFiveA + ((countA % 5) / 3) * offerThreeA + ((countA % 5) % 3) * priceA;
-
-     int freeB = countE / 2;
-
-     if(countB > freeB)
-     {
-         countB -= freeB;
-     }
-     else {
-         countB = 0;
-     }
-
-     total+= (countB / 2) * offerB + (countB % 2) * priceB;
-
-     total += countC * priceC + countD * priceD + countE * priceE;
-
-     total += ((countF / 3) * 2 + (countF % 3)) * priceF;
-
-     return total;
+    if(basket.containsKey('E')){
+        int freeB = basket.get('E') / 2;
+        basket.computeIfPresent('B', (k, v) -> Math.max(v - freeB, 0);
     }
+
 }
+}
+
 
 
