761f44b289ffe5854708ba34c1d6eead4614ac8a
sbx_user1051 <sbx_user1051@169.254.49.229>
2024-04-11 11:17:51 +0000

    Thu Apr 11 11:17:51 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,9 +7,9 @@ public class CheckoutSolution {
 
     public Integer checkout(String skus) {
 
-        int priceA = 50, priceB = 30, priceC = 20, priceD = 15, priceE = 40;
+        int priceA = 50, priceB = 30, priceC = 20, priceD = 15, priceE = 40, priceF = 10;
         int offerThreeA = 130, offerFiveA = 200, offerB = 45;
-        int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0;
+        int countA = 0, countB = 0, countC = 0, countD = 0, countE = 0, countF = 0;
 
         for(char skusChr : skus.toCharArray()){
             switch (skusChr){
@@ -18,6 +18,7 @@ public class CheckoutSolution {
                 case 'C' -> countC++;
                 case 'D' -> countD++;
                 case 'E' -> countE++;
+                case 'F' -> countF++;
                 default -> { return -1; }
             }
 
@@ -28,6 +29,14 @@ public class CheckoutSolution {
      total += (countA / 5) * offerFiveA + ((countA % 5) / 3) * offerThreeA + ((countA % 5) % 3) * priceA;
 
      int freeB = countE / 2;
+     int freeF = 0;
+
+     if(countF > 3){
+
+         freeF = countF / 2;
+
+         countF -= freeF;
+     }
 
      if(countB > freeB)
      {
@@ -41,7 +50,9 @@ public class CheckoutSolution {
 
      total += countC * priceC + countD * priceD + countE * priceE;
 
+     total += countF * priceF;
 
      return total;
     }
 }
+
